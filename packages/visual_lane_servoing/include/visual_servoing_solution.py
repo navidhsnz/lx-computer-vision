a5bede3fd7248b48181ad3d0853abaceedb55803
navid <navid.hsnz@gmail.com>
2024-11-07 20:32:28 -0500

    latest update

--- packages/visual_lane_servoing/include/visual_servoing_solution.py
+++ packages/visual_lane_servoing/include/visual_servoing_solution.py
@@ -17,7 +17,7 @@ def get_steer_matrix_left_lane_markings(shape: Tuple[int, int]) -> np.ndarray:
     """
 
     # TODO: implement your own solution here
-    steer_matrix_left = np.ones(img.shape, dtype=np.uint8)
+    steer_matrix_left = np.ones(shape, dtype=np.uint8)
     return steer_matrix_left
 
 
@@ -32,7 +32,7 @@ def get_steer_matrix_right_lane_markings(shape: Tuple[int, int]) -> np.ndarray:
     """
 
     # TODO: implement your own solution here
-    steer_matrix_right = -np.ones(img.shape, dtype=np.uint8)
+    steer_matrix_right = -np.ones(shape, dtype=np.uint8)
     # ---
     return steer_matrix_right
 
